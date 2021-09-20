cec5f22a5d5d83a974d8d765aa57af86d1a056b2
mktcode <kontakt@markus-kottlaender.de>
2021-09-20 15:16:51 +0200

    refactored address book as GithubDataReceiver

--- src/GithubUserAddressBook.sol
+++ src/GithubUserAddressBook.sol
@@ -1,23 +1,44 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.7;
 
-import './GithubWorkflowClient.sol';
+import './GithubDataReceiver.sol';
 
-contract GithubUserAddressBook is GithubWorkflowClient {
+contract GithubUserAddressBook is GithubDataReceiver {
   mapping(string => address) githubUserAddresses;
+  mapping(address => string) githubUserIds;
+  mapping(uint256 => string) verificationRequests;
 
-  constructor(string memory _workflowHash) {
-    registerGithubWorkflow(msg.sender, 'verify', _workflowHash, 10000000000000000);
+  constructor(address _oracle, address _signer, uint256 _fee) {
+    githubOracleFee = _fee;
+    setGithubOracle(_oracle);
+    setGithubSigner(_signer);
   }
 
-  function requestVerification(string calldata _githubUserId) payable public {
-    githubWorkflowRequest('verify', _githubUserId);
+  function requestAddressVerification(string calldata _githubUserId) payable public {
+    uint256 requestId = signedGithubOracleRequest(
+      this.verifyAddress.selector,
+      'User { gists(last: 1) { nodes { files(limit: 1) { text } } } }',
+      _githubUserId
+    );
+
+    verificationRequests[requestId] = _githubUserId;
   }
 
-  event VerifyEvent(uint256 requestId);
-  function verify(uint256 _requestId) public githubWorkflowResponse(_requestId) {
-    githubUserAddresses[githubWorkflowRequests[_requestId].githubUserId] = githubWorkflowRequests[_requestId].sender;
+  event VerifyAddressEvent(uint256 requestId, string githubUserId, address account);
+  function verifyAddress(uint256 _requestId, address _value, bytes memory _signature)
+    public
+    signedGithubOracleResponseAddress(_requestId, _value, _signature)
+  {
+    require(_value == githubDataRequests[_requestId].sender, "Address does not match the requested one.");
+    githubUserAddresses[verificationRequests[_requestId]] = _value;
+    emit VerifyAddressEvent(_requestId, verificationRequests[_requestId], _value);
+  }
 
-    emit VerifyEvent(_requestId);
+  function get(string calldata _githubUserId) public view returns(address payable) {
+    return payable(githubUserAddresses[_githubUserId]);
+  }
+
+  function getId(address _address) public view returns(string memory) {
+    return githubUserIds[_address];
   }
 }
